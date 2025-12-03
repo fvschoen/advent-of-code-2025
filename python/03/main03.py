2a078c14059e10748f3cc7b97d975151224eb836
fvschoen <frank.schoeneman@gmail.com>
2025-12-03 17:04:11 -0500

    day 3, part two

--- python/03/main03.py
+++ python/03/main03.py
@@ -1,5 +1,6 @@
 from typing import List
 from collections import deque
+from functools import lru_cache
 
 def format_banks(banks: List[str]) -> List[List[int]]:
     return [list(map(int, list(bank.strip()))) for bank in banks]
@@ -31,6 +32,29 @@ def part01_batteries_on(banks: List[List[int]]) -> int:
     
     return sum(activate_bank(bank) for bank in banks)
 
+def part02_batteries_on(banks: List[List[int]], k: int) -> int:
+
+    def activate_bank(bank: str, k: int) -> int:
+        @lru_cache(maxsize=None)
+        def build_max(curr: int, remaining: int) -> str:
+
+            # base cases
+            if not remaining:
+                return ''
+            if curr == len(bank):
+                return ''
+                        
+            # recursive cases
+            use_battery = bank[curr] + build_max(curr + 1, remaining - 1) \
+                if remaining <= len(bank) - curr else ''
+            skip_battery = build_max(curr + 1, remaining)
+
+            return max(use_battery, skip_battery)
+            
+        return int(build_max(0, k))
+
+    return sum(activate_bank(''.join(map(str, bank)), k) for bank in banks)
+
 
 if __name__ == "__main__":
 
@@ -38,6 +62,10 @@ if __name__ == "__main__":
         banks = f.readlines()
 
     banks = format_banks(banks)
+    k = 12
 
     print ("""--- Part One ---""")
     print("Total output Joltage:", part01_batteries_on(banks), "\n")
+
+    print ("""--- Part Two ---""")
+    print("Total output Joltage:", part02_batteries_on(banks, k), "\n")
